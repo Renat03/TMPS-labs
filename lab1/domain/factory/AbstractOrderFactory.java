02eaf21858ad133a60978cc177a778f716d9c2e4
Renat03 <grsrenat@gmail.com>
2024-10-27 23:21:05 +0200

    Add report for laboratory 1

--- lab1/domain/factory/AbstractOrderFactory.java
+++ lab1/domain/factory/AbstractOrderFactory.java
@@ -5,3 +5,5 @@ import domain.models.order.Order;
 public abstract class AbstractOrderFactory {
     public abstract Order createOrder();
 }
+
+
--- lab1/domain/models/builder/OrderBuilder.java
+++ lab1/domain/models/builder/OrderBuilder.java
@@ -43,3 +43,4 @@ public class OrderBuilder {
         return order;
     }
 }
+
